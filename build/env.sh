af78297d114aee0188cdd09f3729956f031e8d8a
Jianjun Cui <j0904.cui@gmail.com>
2025-05-20 15:50:27 +0200

    build test

--- build/env.sh
+++ build/env.sh
@@ -3,7 +3,7 @@
 # Exit if any command fails
 set -e
 
-  export DB_POSTGRES_URL="postgresql://pg:password@127.0.0.1:5433/postgres"
+  export DB_POSTGRES_URL="postgresql://pg:password@:5433/postgres"
 
   export REDIS_HOST="127.0.0.1:6380"
 
